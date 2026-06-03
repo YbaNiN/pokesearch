b044b5d58d4bbf2ddaa95b353c61fcc4871f6436
YbaNiN <ybananaliza@gmail.com>
2026-06-03 11:54:00 +0200

    Add PokedexScreen route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { StartScreen } from '@/pages/StartScreen';
 import { GameScreen } from '@/pages/GameScreen';
 import { ResultScreen } from '@/pages/ResultScreen';
 import { RankingScreen } from '@/pages/RankingScreen';
+import { PokedexScreen } from '@/pages/PokedexScreen';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Route path="/play" element={<GameScreen />} />
       <Route path="/result" element={<ResultScreen />} />
       <Route path="/ranking" element={<RankingScreen />} />
+      <Route path="/pokedex" element={<PokedexScreen />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
